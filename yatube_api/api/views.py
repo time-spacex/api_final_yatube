4ca838c9a29619e574d50a6e292b69a0a3af450f
Роман Клешнев <time-space@yandex.ru>
2024-02-02 16:12:38 +0300

    Fix get_post() method in CommentViewSet

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -44,17 +44,17 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly)
 
-    def get_post(self, post_id):
-        return get_object_or_404(Post, pk=post_id)
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
 
     def get_queryset(self):
         """Метод API для получения querysetа комметариев."""
-        post = self.get_post(post_id=self.kwargs.get('post_id'))
+        post = self.get_post()
         return post.comments.all()
 
     def perform_create(self, serializer):
         """Метод API для создания комментария."""
-        post = self.get_post(post_id=self.kwargs.get('post_id'))
+        post = self.get_post()
         serializer.save(author=self.request.user, post=post)
 
 
